baf9df7c3e4d09caf2a65402e84b12edbafcb770
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-09-17 16:21:50 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr ) {
+ let res = Array.isArray(arr);
+  if (res !== true) {
+    throw new RangeError();
+  } else {
+    let g = []
+    for (let i = 0; i <= arr.length - 1; i++) {
+      if (arr[i] == '--discard-next') {
+        delete arr[i + 1];
+      } else if (arr[i] == '--discard-prev') {
+        g.pop();
+      } else if (arr[i] == '--double-next') {
+        g.push(arr[i + 1]);
+
+      } else if (arr[i] == '--double-prev') {
+        g.push(arr[i - 1]);
+
+      } else {
+        g.push(arr[i]);
+      }
+    }
+    return g.filter((el) => el !== undefined);
+  }
 }
