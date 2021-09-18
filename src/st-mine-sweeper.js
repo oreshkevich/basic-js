82ca75cf6df1fca92bb2c24735a9b0b0fcae5ff0
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-09-18 20:47:16 +0300

    Update st-mine-sweeper.js

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,43 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+ const arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    arr.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      let g = 0;
+      if (i < matrix.length - 1) {
+        if (matrix[i + 1][j]) {
+          g += 1;
+        }
+        if (matrix[i + 1][j + 1]) {
+          g += 1;
+        }
+        if (matrix[i + 1][j - 1]) {
+          g += 1;
+        }
+      }
+      if (i > 0) {
+        if (matrix[i - 1][j]) {
+          g += 1;
+        }
+        if (matrix[i - 1][j + 1]) {
+          g += 1;
+        }
+        if (matrix[i - 1][j - 1]) {
+          g += 1;
+        }
+      }
+      if (matrix[i][j - 1]) {
+        g += 1;
+      }
+      if (matrix[i][j + 1]) {
+        g += 1;
+      }
+
+      arr[i].push(g);
+    }
+  }
+  return arr;
 }
